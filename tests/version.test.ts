83b57668e680c79c5a30951dfc37f68ed3037edc
Vladimir Aranovsky <v.aranovsky@gmail.com>
2022-06-16 19:40:32 +0300

    Fix lint issue

--- tests/version.test.ts
+++ tests/version.test.ts
@@ -1,5 +1,5 @@
 import { version } from "../package.json";
 
-test('check project version is current', () => {
+test("check project version is current", () => {
   expect(version).toBe("1.0.0");
 });
